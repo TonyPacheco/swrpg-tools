cc2bc61dd4319b2e86ac4ef60f47ba07b99d97ae
Tony Pacheco <tonypacheco.cillis@gmail.com>
2026-01-11 23:06:16 -0800

    Add character management

---
+++ Navigator.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace CharacterSheet
+{
+    public class Navigator
+    {
+        private readonly NavigationManager _nav;
+
+        public Navigator(NavigationManager nav)
+        {
+            _nav = nav;
+        }
+
+        public void ToCharacterSheet() => _nav.NavigateTo("/");
+        public void ToManageCharacters() => _nav.NavigateTo("/manageCharacters");
+    }
+}
